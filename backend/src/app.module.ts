a4ecdd2cfb8246f012c95fe0218b261df7845e51
Егор <posolvzopulox@mail.ru>
2025-04-28 00:21:37 +0300

    Initial project skeleton

---
+++ backend/src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    // TODO: add AuthModule, UsersModule, TelegramModule, OdataModule, SiteApiModule, CronModule
+  ],
+})
+export class AppModule {}
